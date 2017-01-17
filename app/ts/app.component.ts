4c53573e5fcba4fd497639a6b158bf6f7ea9e2a6
jrivera-technisys <jrivera@technisys.com>
2017-01-17 18:35:57 -0300

    code followed up to last tutorial

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,23 @@
 import {Component} from 'angular2/core';
+import {Config} from './config.service'; 
+import {Video} from  './video';
+import {PlayListComponent} from './playlist.component';
+
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlayListComponent]
 })
 
-export class AppComponent {}
+export class AppComponent {
+    mainHeading = Config.MAIN_HEADING;
+    videos:Array<Video>;
+
+    constructor(){
+        this.videos = [
+            new Video(1,"Angular 2 for Beginners","f8qBeaGe2S4","Using Angular 2 for the first time!"),
+            new Video(2,"Angular 2 for Beginners Next part","bKWDKmHvF78","Second video of the series.")
+        ]
+    }
+}
